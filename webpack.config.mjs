963e6a44ce6a3fb3666b3af21ca7de2df0e6dd19
Vevina Lin <han.qi.lin@showmore.cc>
2025-01-05 03:44:52 +0800

    webpack2

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -9,13 +9,13 @@ export default {
     output: {
         filename: 'main.js',
         path: path.resolve('./dist'),
-        clean: true
+        clean: true,
     },
     target: 'web',
     devServer: {
         port: '4000',
         static: {
-          directory: path.resolve('./public')
+            directory: path.resolve('./public'),
         },
         open: true,
         hot: true,
@@ -24,49 +24,49 @@ export default {
     resolve: {
         extensions: ['.js', '.jsx', '.json'],
     },
-    
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './index.html',
             minify: {
                 collapseWhitespace: true,
-                removeComments: true
-            }
+                removeComments: true,
+            },
         }),
         new MiniCssExtractPlugin({
-            filename: 'src/index.css'
+            filename: 'styles.css',
         }),
         new CopyWebpackPlugin({
             patterns: [
                 { from: 'images/*.jpg', to: 'images/[name][ext]' },
                 { from: 'images/*.png', to: 'images/[name][ext]' },
-
             ],
         }),
-
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader', // 使用 PostCSS
+                ],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'images',
+                type: 'asset/resource', // 修正为资源加载类型
                 generator: {
-                    filename: 'images/[name][ext]'
-                }
+                    filename: 'images/[name][ext]',
+                },
             },
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
-                }
+                },
             },
-        ]
+        ],
     },
-    
 };
